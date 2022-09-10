d26a2e8f03d26b515edaf3b48e6b8a01942eb949
jpetliuk <jeremias.petliuk@gmail.com>
2022-09-10 19:33:27 -0300

    sedes terminado, falta mapear

--- src/components/Sedes/Sedes.js
+++ src/components/Sedes/Sedes.js
@@ -2,6 +2,7 @@ import "./Sedes.style.css";
 import instagram from "../../assets/img/icons/instagram.png";
 import facebook from "../../assets/img/icons/facebook.png";
 import whatsapp from "../../assets/img/icons/whatsapp.png";
+import maps from "../../assets/img/icons/maps-and-flags.png";
 
 const Sedes = () => {
   return (
@@ -22,6 +23,9 @@ const Sedes = () => {
                   <img src={whatsapp} alt="whatsapp" />
                 </a>
                 <a href="/">
+                  <img src={maps} alt="maps" />
+                </a>
+                <a href="/">
                   <img src={instagram} alt="instagram" />
                 </a>
                 <a href="/">
@@ -31,30 +35,38 @@ const Sedes = () => {
             </div>
           </div>
           <div className="sedesHover">
-            <h2>FLORIDA</h2>
-            <p>Av. Gral. José de san martín 2462</p>
-            <button>CONOCER</button>
-            <div className="sedesMediaLinks">
-              <a href="/">
-                <img src={whatsapp} alt="whatsapp" />
-              </a>
-              <a href="/">
-                <img src={instagram} alt="instagram" />
-              </a>
-              <a href="/">
-                <img src={facebook} alt="facebook" />
-              </a>
+            <div className="sedesHoverContent">
+              <h2>FLORIDA</h2>
+              <p>Av. Gral. José de san martín 2462</p>
+              <button>CONOCER</button>
+              <div className="sedesMediaLinks">
+                <a href="/">
+                  <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
+                </a>
+                <a href="/">
+                  <img src={instagram} alt="instagram" />
+                </a>
+                <a href="/">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
         <div className="sedeBlock">
           <div id="sedeMaipu" className="sedeStylesheet">
             <div className="sedeStylesheetContent">
-              <h2>FLORIDA</h2>
-              <p>Av. Gral. José de san martín 2462</p>
+              <h2>MAIPU</h2>
+              <p>Av. Maipú 574</p>
               <div className="sedesMediaLinks">
                 <a href="/">
                   <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
                 </a>
                 <a href="/">
                   <img src={instagram} alt="instagram" />
@@ -66,30 +78,38 @@ const Sedes = () => {
             </div>
           </div>
           <div className="sedesHover">
-            <h2>FLORIDA</h2>
-            <p>Av. Gral. José de san martín 2462</p>
-            <button>CONOCER</button>
-            <div className="sedesMediaLinks">
-              <a href="/">
-                <img src={whatsapp} alt="whatsapp" />
-              </a>
-              <a href="/">
-                <img src={instagram} alt="instagram" />
-              </a>
-              <a href="/">
-                <img src={facebook} alt="facebook" />
-              </a>
+            <div className="sedesHoverContent">
+              <h2>MAIPU</h2>
+              <p>Av. Maipú 574</p>
+              <button>CONOCER</button>
+              <div className="sedesMediaLinks">
+                <a href="/">
+                  <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
+                </a>
+                <a href="/">
+                  <img src={instagram} alt="instagram" />
+                </a>
+                <a href="/">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
         <div className="sedeBlock">
           <div id="sedeNunez" className="sedeStylesheet">
             <div className="sedeStylesheetContent">
-              <h2>FLORIDA</h2>
-              <p>Av. Gral. José de san martín 2462</p>
+              <h2>NUÑEZ</h2>
+              <p>Av. Cabildo 3030</p>
               <div className="sedesMediaLinks">
                 <a href="/">
                   <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
                 </a>
                 <a href="/">
                   <img src={instagram} alt="instagram" />
@@ -101,30 +121,38 @@ const Sedes = () => {
             </div>
           </div>
           <div className="sedesHover">
-            <h2>FLORIDA</h2>
-            <p>Av. Gral. José de san martín 2462</p>
-            <button>CONOCER</button>
-            <div className="sedesMediaLinks">
-              <a href="/">
-                <img src={whatsapp} alt="whatsapp" />
-              </a>
-              <a href="/">
-                <img src={instagram} alt="instagram" />
-              </a>
-              <a href="/">
-                <img src={facebook} alt="facebook" />
-              </a>
+            <div className="sedesHoverContent">
+              <h2>NUÑEZ</h2>
+              <p>Av. Cabildo 3030</p>
+              <button>CONOCER</button>
+              <div className="sedesMediaLinks">
+                <a href="/">
+                  <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
+                </a>
+                <a href="/">
+                  <img src={instagram} alt="instagram" />
+                </a>
+                <a href="/">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
         <div className="sedeBlock">
           <div id="sedeSanFernando" className="sedeStylesheet">
             <div className="sedeStylesheetContent">
-              <h2>FLORIDA</h2>
-              <p>Av. Gral. José de san martín 2462</p>
+              <h2>SAN FERNANDO</h2>
+              <p>Av. Constitución 1635</p>
               <div className="sedesMediaLinks">
                 <a href="/">
                   <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
                 </a>
                 <a href="/">
                   <img src={instagram} alt="instagram" />
@@ -136,30 +164,38 @@ const Sedes = () => {
             </div>
           </div>
           <div className="sedesHover">
-            <h2>FLORIDA</h2>
-            <p>Av. Gral. José de san martín 2462</p>
-            <button>CONOCER</button>
-            <div className="sedesMediaLinks">
-              <a href="/">
-                <img src={whatsapp} alt="whatsapp" />
-              </a>
-              <a href="/">
-                <img src={instagram} alt="instagram" />
-              </a>
-              <a href="/">
-                <img src={facebook} alt="facebook" />
-              </a>
+            <div className="sedesHoverContent">
+              <h2>SAN FERNANDO</h2>
+              <p>Av. Constitución 1635</p>
+              <button>CONOCER</button>
+              <div className="sedesMediaLinks">
+                <a href="/">
+                  <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
+                </a>
+                <a href="/">
+                  <img src={instagram} alt="instagram" />
+                </a>
+                <a href="/">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
         <div className="sedeBlock">
           <div id="sedeTigre" className="sedeStylesheet">
             <div className="sedeStylesheetContent">
-              <h2>FLORIDA</h2>
-              <p>Av. Gral. José de san martín 2462</p>
+              <h2>TIGRE</h2>
+              <p>Av. Cazón 749</p>
               <div className="sedesMediaLinks">
                 <a href="/">
                   <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
                 </a>
                 <a href="/">
                   <img src={instagram} alt="instagram" />
@@ -171,30 +207,38 @@ const Sedes = () => {
             </div>
           </div>
           <div className="sedesHover">
-            <h2>FLORIDA</h2>
-            <p>Av. Gral. José de san martín 2462</p>
-            <button>CONOCER</button>
-            <div className="sedesMediaLinks">
-              <a href="/">
-                <img src={whatsapp} alt="whatsapp" />
-              </a>
-              <a href="/">
-                <img src={instagram} alt="instagram" />
-              </a>
-              <a href="/">
-                <img src={facebook} alt="facebook" />
-              </a>
+            <div className="sedesHoverContent">
+              <h2>TIGRE</h2>
+              <p>Av. Cazón 749</p>
+              <button>CONOCER</button>
+              <div className="sedesMediaLinks">
+                <a href="/">
+                  <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
+                </a>
+                <a href="/">
+                  <img src={instagram} alt="instagram" />
+                </a>
+                <a href="/">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
         <div className="sedeBlock">
           <div id="sedeSanBernardo" className="sedeStylesheet">
             <div className="sedeStylesheetContent">
-              <h2>FLORIDA</h2>
-              <p>Av. Gral. José de san martín 2462</p>
+              <h2>SAN BERNARDO</h2>
+              <p>Av. San Bernardo 866</p>
               <div className="sedesMediaLinks">
                 <a href="/">
                   <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
                 </a>
                 <a href="/">
                   <img src={instagram} alt="instagram" />
@@ -206,19 +250,24 @@ const Sedes = () => {
             </div>
           </div>
           <div className="sedesHover">
-            <h2>FLORIDA</h2>
-            <p>Av. Gral. José de san martín 2462</p>
-            <button>CONOCER</button>
-            <div className="sedesMediaLinks">
-              <a href="/">
-                <img src={whatsapp} alt="whatsapp" />
-              </a>
-              <a href="/">
-                <img src={instagram} alt="instagram" />
-              </a>
-              <a href="/">
-                <img src={facebook} alt="facebook" />
-              </a>
+            <div className="sedesHoverContent">
+              <h2>SAN BERNARDO</h2>
+              <p>Av. San Bernardo 866</p>
+              <button>CONOCER</button>
+              <div className="sedesMediaLinks">
+                <a href="/">
+                  <img src={whatsapp} alt="whatsapp" />
+                </a>
+                <a href="/">
+                  <img src={maps} alt="maps" />
+                </a>
+                <a href="/">
+                  <img src={instagram} alt="instagram" />
+                </a>
+                <a href="/">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
